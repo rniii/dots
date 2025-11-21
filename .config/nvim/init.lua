340bb17f21752ff2c8220cbf01ce8d1df53b0f2b
rini <nil@dissoc.cc>
2025-11-21 19:02:55 -0300

    gwa gwa

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -12,7 +12,6 @@ set title titlestring=%t%(\ (%{fnamemodify(getcwd(),\":~\")})%)%(\ -\ nvim%)
 
 set smartcase ignorecase
 set completeopt=menuone,longest
-set nofoldenable foldmethod=marker
 set undofile sessionoptions+=globals
 ]]
 
@@ -22,14 +21,15 @@ vim.g.mapleader = ","
 require "paq" {
   "nvim-tree/nvim-web-devicons",
 
-  "nvim-lualine/lualine.nvim", "akinsho/bufferline.nvim",
-  "Bekaboo/dropbar.nvim",      "lewis6991/satellite.nvim",
-  "lewis6991/gitsigns.nvim",   "j-hui/fidget.nvim",
+  "nvim-lualine/lualine.nvim", "Bekaboo/dropbar.nvim",
+  "lewis6991/satellite.nvim",  "lewis6991/gitsigns.nvim",
+  "j-hui/fidget.nvim",         "chrisgrieser/nvim-origami",
 
   { "nvim-treesitter/nvim-treesitter", build = ":TSUpdate" },
   "RRethy/vim-illuminate", "brenoprata10/nvim-highlight-colors",
 
   "neovim/nvim-lspconfig", "b0o/schemastore.nvim",
+  "marilari88/twoslash-queries.nvim",
 
   "windwp/nvim-autopairs", "windwp/nvim-ts-autotag",
   "tpope/vim-repeat",      "tpope/vim-commentary",
@@ -42,6 +42,7 @@ require "paq" {
   "hrsh7th/nvim-cmp",         "hrsh7th/cmp-nvim-lsp",
   "saadparwaiz1/cmp_luasnip", "L3MON4D3/LuaSnip",
 
+  "andweeb/presence.nvim",
   "Apeiros-46B/uiua.vim",
   "sheerun/vim-polyglot",
   "justinmk/vim-dirvish",
@@ -76,24 +77,26 @@ end
 require("lualine").setup {
   options = {
     theme = "thorns",
-    section_separators = " ",
+    section_separators = "",
     component_separators = "",
   },
   sections = {
     lualine_a = { "mode" },
     lualine_b = { "branch", { "diff", source = diff_source } },
-    lualine_c = { { "filename", newfile_status = true }, "diagnostics" },
+    lualine_c = { { "filename", path = 1, newfile_status = true }, "diagnostics" },
     lualine_x = { { "filetype", icons_enabled = false } },
     lualine_y = { "encoding", { "fileformat", symbols = ff_symbols } },
     lualine_z = { "progress", "location" },
   },
-}
-
-require("bufferline").setup {
-  options = {
-    indicator = { style = "none" },
-    diagnostics = "nvim_lsp",
-    show_buffer_close_icons = false,
+  tabline = {
+    lualine_c = {
+      {
+        "buffers",
+        symbols = { alternate_file = "" },
+        max_length = function () return vim.o.columns end,
+        padding = 4,
+      },
+    },
   }
 }
 
@@ -122,23 +125,29 @@ require("telescope").setup {
   },
 }
 
+vim.o.foldlevel = 99
+vim.o.foldlevelstart = 99
+
+require("origami").setup {
+  autoFold = {
+    enabled = true,
+    kinds = { "imports" },
+  },
+}
+
 --- Highlighting ---------------------------------------------------------- {{{1
 require("nvim-treesitter.configs").setup {
   ensure_installed = {
-    "embedded_template", "javascript", "markdown_inline", "purescript", "typescript",
-    ----------- ----------- ----------- ----------- ----------- ----------- -----------
-    "bash",     "cpp",      "c_sharp",  "c",        "css",      "doxygen",  "d",
-    "gleam",    "glsl",     "go",       "haskell",  "html",     "java",     "jsdoc",
-    "jsonc",    "json",     "lua",      "markdown", "ocaml",    "python",   "regex",
-    "ruby",     "rust",     "scheme",   "scss",     "tcl",      "toml",     "tsx",
-    "vimdoc",   "vim",      "vue",      "yaml",
+    "javascript", "typescript", "tsx", "jsdoc", "html", "css", "vue",
+    "json", "jsonc", "yaml", "toml", "markdown", "markdown_inline",
+    "c", "cpp", "doxygen", "d", "go", "glsl", "haskell", "ocaml", "rust",
+    "lua", "bash", "ruby", "python", "tcl", "vim", "vimdoc",
+    "java", "kotlin",
   },
   highlight = { enable = true },
   autotag = { enable = true },
   indent = { enable = true },
 }
-
-vim.treesitter.language.register("ruby", "crystal")
 
 require("illuminate").configure {
   providers = {"lsp", "treesitter"}
@@ -147,30 +156,17 @@ require("illuminate").configure {
 require("nvim-highlight-colors").setup {}
 
 --- LSP ------------------------------------------------------------------- {{{1
-local lspconfig = require "lspconfig"
-
-lspconfig.util.default_config.settings = {
-  ["rust-analyzer"] = {
-    check = { command = "clippy" },
-    imports = {
-      granularity = { group = "module", enforce = true },
-    },
-  },
-  ["ccls"] = {
-    diagnostics = { onOpen = 100 },
-  },
+require("twoslash-queries").setup {
+  highlight = "Special"
 }
-
--- lspconfig.util.default_config.capabilities = require("cmp_nvim_lsp").default_capabilities()
 
 vim.lsp.enable {
-  "csharp_ls", "emmet_language_server", "kotlin_lsp", "purescriptls", "rust_analyzer",
-  ----------- ----------- ----------- ----------- ----------- ----------- -----------
-  "astro",    "ccls",     "efm",      "elmls",    "gopls",    "hls",      "lua_ls",
-  "mesonlsp", "ocamllsp", "ols",      "pylsp",    "qmlls",    "serve_d",  "taplo",
-  "ts_ls",    "uiua",     "volar",    "zls",      "cssls",    "eslint",   "jsonls",
-  "html",
+  "ts_ls", "eslint", "html", "emmet_language_server", "cssls", "jsonls",
+  "ccls", "mesonlsp", "gopls", "hls", "ocamllsp", "rust_analyzer", "zls",
+  "lua_ls", "efm", "pylsp", "uiua", "kotlin_lsp",
 }
+
+vim.lsp.config("ccls", { settings = { ccls = { diagnostics = { onOpen = 100 } } } })
 
 vim.lsp.config("efm", {
   filetypes = { "sh", "bash", "zsh" },
@@ -195,6 +191,20 @@ vim.lsp.config("jsonls", {
   end
 })
 
+vim.lsp.config("ts_ls", {
+  on_attach = function (client, bufnr)
+    require("twoslash-queries").attach(client, bufnr)
+  end
+})
+
+vim.lsp.config("eslint", {
+  settings = {
+    rulesCustomizations = {
+      { rule = "@stylistic/*", severity = "off", fixable = true },
+    },
+  },
+})
+
 --- Editing --------------------------------------------------------------- {{{1
 local npairs = require "nvim-autopairs"
 
@@ -213,9 +223,6 @@ conform.setup {
     ocaml = {"ocamlformat"},  python  = {"black"},
     rust  = {"rustfmt"},      scss    = {"prettier"},
     vue   = {"prettier"},     zig     = {"zigfmt"},
-
-    javascript = {"dprint"}, javascriptreact = {"dprint"},
-    typescript = {"dprint"}, typescriptreact = {"dprint"},
   }
 }
 
@@ -243,9 +250,8 @@ map <Leader>gt <Cmd>Telescope lsp_type_definitions<CR>
 map <Leader>l <Cmd>Telescope find_files<CR>
 map <Leader>p <Cmd>Telescope live_grep<CR>
 
-map gb <Cmd>BufferLinePick<CR>
-map <C-k> <Cmd>BufferLineCyclePrev<CR>
-map <C-j> <Cmd>BufferLineCycleNext<CR>
+map <C-k> <Cmd>bprev<CR>
+map <C-j> <Cmd>bnext<CR>
 
 nmap ga <Plug>(EasyAlign)
 vmap ga <Plug>(EasyAlign)
@@ -257,10 +263,14 @@ tnoremap <C-w>k <C-\><C-N><C-w>k
 tnoremap <C-w>l <C-\><C-N><C-w>l
 
 map <Leader>f <Cmd>Conform<CR>
-autocmd FileType javascript,javascriptreact,typescript,typescriptreact map <Leader>f <Cmd>silent! EslintFixAll<CR><Cmd>Conform<CR>
+autocmd FileType javascript,javascriptreact,typescript,typescriptreact map <Leader>f <Cmd>silent! LspEslintFixAll<CR><Cmd>Conform<CR>
 
 autocmd FileType cpp setlocal commentstring=//\ %s
 ]]
+
+vim.api.nvim_create_user_command("LazyGit", function ()
+  vim.system({"alacritty", "-e", "zsh", "-c", "bspc node -z top 0 70 && lazygit"})
+end, { nargs = 0 })
 
 --- Completion ------------------------------------------------------------ {{{1
 local cmp = require "cmp"
@@ -322,3 +332,48 @@ vim.api.nvim_create_autocmd("BufWritePre", {
     require("session_manager").save_current_session()
   end
 })
+
+local presence_url = "https://raw.githubusercontent.com/iCrawl/discord-vscode/refs/heads/master/assets/icons/"
+
+local presence_assets = {}
+
+for key, value in pairs({
+  js    = "JavaScript", ts   = "TypeScript",
+  jsx   = "JSX",        tsx  = "TSX",
+  html  = "HTML",       css  = "CSS",
+  vue   = "Vue",        json = "JSON",
+  jsonc = "JSON",       yaml = "YAML",
+  toml  = "TOML",       md   = { "Markdown", "markdown" },
+  c     = "C",          cxx  = { "C++", "cpp" },
+  cpp   = "C++",        cc   = { "C++", "cpp" },
+  d     = "D",          go   = "Go",
+  glsl  = "GLSL",       hs   = { "Haskell", "haskell" },
+  rs    = "Rust",       ml   = { "OCaml", "ocaml" },
+  lua   = "Lua",        bash = "Bash",
+  ruby  = "Ruby",       py   = { "Python", "python" },
+  tcl   = "TCL",
+}) do
+  local name, file
+  if type(value) == "string" then
+    name, file = value, key
+  else
+    name, file = value[1], value[2]
+  end
+
+  presence_assets[key] = { name, presence_url .. file .. ".png" }
+end
+
+require("presence").setup {
+  main_image = "file",
+  file_assets = presence_assets,
+  buttons = function (_, repo_url)
+    if not repo_url then return end
+
+    repo_url = string.gsub(repo_url, "^(.+)@(.+):(.+)$", "https://%2/%3")
+    repo_url = string.gsub(repo_url, ".git$", "")
+
+    return {
+      { label = "View Repository", url = repo_url }
+    }
+  end
+}
